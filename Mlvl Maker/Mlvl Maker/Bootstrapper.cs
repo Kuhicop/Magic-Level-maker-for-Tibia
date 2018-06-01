01d8ee7ad2496c79c9ec9aca6826996740153d08
Razonek <raging@onet.eu>
2018-06-02 00:46:18 +0200

    Framework Caliburn.Mirco

---
+++ Mlvl Maker/Mlvl Maker/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+
+namespace Mlvl_Maker
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MagicLevelMakerViewModel>();
+        }
+
+    }
+}
---
+++ Mlvl Maker/Mlvl Maker/MagicLevelMakerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Caliburn.Micro;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mlvl_Maker
+{
+    public class MagicLevelMakerViewModel : Screen
+    {
+        public MagicLevelMakerViewModel()
+        {
+            this.DisplayName = "Magic Level Maker";
+        }
+
+
+    }
+}
